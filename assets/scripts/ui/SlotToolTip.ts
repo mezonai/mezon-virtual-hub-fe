9630c394458255cbe6280556afbab2036529b75a
thyannguyentran <an.nguyentranthy@ncc.asia>
2025-06-17 13:49:27 +0700

    refactor code

--- assets/scripts/ui/SlotToolTip.ts
+++ assets/scripts/ui/SlotToolTip.ts
@@ -1,10 +1,11 @@
 import { _decorator, Component, Node, Label, Vec3, UITransform, Director, Prefab, view } from 'cc';
 import { TooltipManager } from './TooltipManager'; // Import TooltipManager
 import { RewardDisplayData } from '../Model/Item';
+import { TooltipHover } from '../utilities/TooltipHover';
 const { ccclass, property } = _decorator;
 
 @ccclass('SlotTooltip')
-export class SlotTooltip extends Component {
+export class SlotTooltip extends TooltipHover {
     private data: RewardDisplayData;
     private tooltipManager: TooltipManager = null;
 
@@ -14,25 +15,25 @@ export class SlotTooltip extends Component {
     }
 
     onLoad() {
-        this.node.on(Node.EventType.MOUSE_ENTER, this._onHoverShow, this);
-        this.node.on(Node.EventType.MOUSE_LEAVE, this._onHoverHide, this);
-        this.node.on(Node.EventType.TOUCH_START, this._onHoverShow, this);
-        this.node.on(Node.EventType.TOUCH_END, this._onHoverHide, this);
+        this.node.on(Node.EventType.MOUSE_ENTER, this.onHoverShow, this);
+        this.node.on(Node.EventType.MOUSE_LEAVE, this.onHoverHide, this);
+        this.node.on(Node.EventType.TOUCH_START, this.onHoverShow, this);
+        this.node.on(Node.EventType.TOUCH_END, this.onHoverHide, this);
     }
 
-    private _onHoverShow() {
+    public override onHoverShow() {
         this.tooltipManager.showGlobalTooltip(this.data, this.node);
     }
 
-    private _onHoverHide() {
+    public override onHoverHide() {
         this.tooltipManager.hideGlobalTooltip();
     }
 
     onDestroy() {
-        this.node.off(Node.EventType.MOUSE_ENTER, this._onHoverShow, this);
-        this.node.off(Node.EventType.MOUSE_LEAVE, this._onHoverHide, this);
-        this.node.off(Node.EventType.TOUCH_START, this._onHoverShow, this);
-        this.node.off(Node.EventType.TOUCH_END, this._onHoverHide, this);
-        this._onHoverHide();
+        this.node.off(Node.EventType.MOUSE_ENTER, this.onHoverShow, this);
+        this.node.off(Node.EventType.MOUSE_LEAVE, this.onHoverHide, this);
+        this.node.off(Node.EventType.TOUCH_START, this.onHoverShow, this);
+        this.node.off(Node.EventType.TOUCH_END, this.onHoverHide, this);
+        this.onHoverHide();
     }
 }
