c38974a888fe549a5a766df473b5950c58ae59b9
thyannguyentran <an.nguyentranthy@ncc.asia>
2025-06-18 17:47:18 +0700

    remove debug log

--- assets/scripts/utilities/ColliderAutoUpdater.ts
+++ assets/scripts/utilities/ColliderAutoUpdater.ts
@@ -12,11 +12,6 @@ export class ColliderAutoUpdater extends Component {
     private resizeDebounceTimer: any = null;
 
     start() {
-        // if (PhysicsSystem2D.instance) {
-        //     PhysicsSystem2D.instance.enable = true;
-        //     PhysicsSystem2D.instance.debugDrawFlags = EPhysics2DDrawFlags.Aabb | EPhysics2DDrawFlags.Pair |EPhysics2DDrawFlags.CenterOfMass |EPhysics2DDrawFlags.Joint |EPhysics2DDrawFlags.Shape;
-        // }
-
         director.on(EVENT_NAME.CANVAS_RESIZE, this.onCanvasResize, this);
         input.on(Input.EventType.MOUSE_UP, this.onMouseUp, this);
     }
