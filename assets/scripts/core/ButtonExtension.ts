76127514438a4681e4d962ebc13bf9b85bcceab0
tam.canhlechi <tam.canhlechi@ncc.asia>
2025-07-25 09:45:36 +0700

    AddButtonExtension

---
+++ assets/scripts/core/ButtonExtension.ts
@@ -0,0 +1,27 @@
+import { Button } from 'cc';
+
+// Khai báo interface mở rộng để TypeScript hiểu có addAsyncListener
+declare module 'cc' {
+    interface Button {
+        addAsyncListener(callback: () => Promise<void>): void;
+    }
+}
+
+Button.prototype.addAsyncListener = function (this: Button, callback: () => Promise<void>) {
+    let isProcessing = false;
+
+    const handler = async () => {
+        console.error("isProcessing", isProcessing);
+        if (isProcessing) return;
+        isProcessing = true;
+        try {
+            await callback();
+        } catch (e) {
+            console.error("Error in async button action:", e);
+        } finally {
+            isProcessing = false;
+        }
+    };
+
+    this.node.on(Button.EventType.CLICK, handler);
+};
