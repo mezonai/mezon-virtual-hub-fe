4675002ce37f137c95c526ac245a71182def9c9b
thyannguyentran <an.nguyentranthy@ncc.asia>
2025-09-12 10:18:23 +0700

    Pet Surrender Popup

--- assets/scripts/PopUp/PopupBattlePet.ts
+++ assets/scripts/PopUp/PopupBattlePet.ts
@@ -20,6 +20,8 @@ import { AnimalType } from '../animal/AnimalController';
 import { PopupPetElementChart } from './PopupPetElementChart';
 import { BGMType, SoundManager } from '../core/SoundManager';
 import { Label } from 'cc';
+import { PopupSelectionMini, SelectionMiniParam } from './PopupSelectionMini';
+import Utilities from '../utilities/Utilities';
 const { ccclass, property } = _decorator;
 @ccclass('PlayerBattleStats')
 export class PlayerBattleStats {
@@ -64,8 +66,21 @@ export class PopupBattlePet extends Component {
             await this.slideSkillButtons.slide(true, 0.3);
         });
         this.surrenderButton.addAsyncListener(async () => {
-            ServerManager.instance.sendSurrenderBattle();
-            await Constants.waitUntil(() => !this.node.activeInHierarchy);
+            const param: SelectionMiniParam = {
+                title: "Chú ý",
+                content: `Bạn có chắc chắn muốn đầu hàng đối thủ!!!`,
+                textButtonLeft: "OK",
+                textButtonRight: "Thôi",
+                textButtonCenter: "",
+                onActionButtonLeft: async () => {
+                    ServerManager.instance.sendSurrenderBattle();
+                    await Constants.waitUntil(() => !this.node.activeInHierarchy);
+                },
+                onActionButtonRight: () => {
+
+                },
+            };
+            PopupManager.getInstance().openAnimPopup("PopupSelectionMini", PopupSelectionMini, param);
         });
         this.hideSkillButton.addAsyncListener(async () => {
             this.hideSkillButton.interactable = false;
